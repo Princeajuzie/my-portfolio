fc1c180bf8210ded4d0e2741b3727d97abdd068b
princeajuzie7 <princeajuzie03@gmail.com>
2024-01-30 13:55:04 +0100

    feat:ui setup on my personal portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
+import { ProfileCard } from "@/components";
 
 export default function Home() {
   return (
-    <main>
-    <div>
-      <h1>hello world</h1>
+    <div className=" p-[15px] bg-[#191923] h-screen w-[100vw] top-0 relative overflow-hidden">
+      <div className="max-w-[1440px] ml-auto mr-auto bg-[#1e1e28] bg-cover w-full h-full relative overflow-hidden">
+        <div className="relative flex flex-nowrap">
+          <main
+            style={{
+              backgroundImage: `url(/resources/images/bg.jpg)`,
+              backgroundPosition: " center",
+              backgroundSize: "cover",
+            }}
+            className="h-[400px]  w-full overflow-hidden  "
+          >
+            <section className="relative bg-gradient-to-b from-[rgba(30,30,40,0.93)] via-[rgba(30,30,40,0.96)] to-[rgba(30,30,40,0.99)]  h-screen w-full">
+              <div className="">
+                <div className="grid grid-cols-2">
+                  <div>
+                    <ProfileCard />
+                  </div>
+                  <div></div>
+                </div>
+              </div>
+            </section>
+          </main>
+        </div>
+      </div>
     </div>
-    </main>
   );
 }
--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,9 +1,20 @@
+import Image from "next/image";
 
-
-export default function ProfileCard() {
+export function ProfileCard() {
   return (
-    <div>
+    <section className="bg-[#20202a] w-[290px] min-w-[290px] shadow-md  relative z-40" style={{height: "calc(100vh - 30px)"}}>
+
+      <div>
+        <div>
+
+        <Image height={200} width={200} src="/resources/images/avatar.jpeg" alt="avatar" className="w-24 h-24 rounded-full mx-auto" />
+        </div>
+      </div>
+      <div></div>
       
-    </div>
+    </section>
   )
 }
+
+
+export default ProfileCard;

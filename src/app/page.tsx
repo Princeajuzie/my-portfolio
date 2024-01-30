799883120f223e60a7d9bedfe95027081605158a
princeajuzie7 <princeajuzie03@gmail.com>
2024-01-30 22:35:52 +0100

    socials added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <div className=" p-[15px] bg-[#191923] h-screen w-[100vw] top-0 relative overflow-hidden">
       <div className="max-w-[1440px] ml-auto mr-auto bg-[#1e1e28] bg-cover w-full h-full relative overflow-hidden">
-        <div className="relative flex flex-nowrap">
+        <div className="relative flex flex-nowrap h-screen">
           <main
             style={{
               backgroundImage: `url(/resources/images/bg.jpg)`,
               backgroundPosition: " center",
               backgroundSize: "cover",
             }}
-            className="h-[400px]  w-full overflow-hidden  "
+            className=" w-full overflow-hidden  "
           >
             <section className="relative bg-gradient-to-b from-[rgba(30,30,40,0.93)] via-[rgba(30,30,40,0.96)] to-[rgba(30,30,40,0.99)]  h-screen w-full">
               <div className="h-screen">

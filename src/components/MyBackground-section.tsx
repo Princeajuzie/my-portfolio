41f8a8e978fc4c841dee5a11428b1ee90d3da573
princeajuzie7 <princeajuzie03@gmail.com>
2024-02-02 22:20:52 +0100

    fix: error

--- src/components/MyBackground-section.tsx
+++ src/components/MyBackground-section.tsx
@@ -2,9 +2,9 @@ import { IoIosArrowForward } from "react-icons/io";
 import Link from "next/link";
 export function MyBackgroundsection() {
   return (
-    <div className="overflow-hidden py-0 px-[30px] relative">
-      <div className="flex  gap-[10px] flex-row">
-        <div className="lg:w-1/2 flex-grow-0 flex-shrink-0 flex-basis-1/2 max-w-[50%]">
+    <div className="overflow-hidden py-0 md:px-[30px] px-[20px] relative">
+      <div className="flex  gap-[20px] flex-col md:flex-row">
+        <div className="lg:w-1/2 flex-grow-0 flex-shrink-0 flex-basis-1/2 max-w-[100%] md:max-w-[50%]">
           <div className="self-center relative mb-[30px]">
             <h4 className="text-[14px] text-[600] text-[#fafafc]">Education</h4>
           </div>
@@ -13,8 +13,8 @@ export function MyBackgroundsection() {
             <div className="relative">
               <div className="bg-[#fafafc] absolute top-4 right-[-4px] w-[23px] h-[23px] opacity-0 rounded-full transition duration-400 ease-in-out"></div>
               <div className="absolute top-[8px] right-0 w-[15px] h-[15px] rounded-full border-[3px] border-solid border-[#ffc107] bg-[#1e1e28] shadow-md"></div>
-              <div className=" mr-[40px] bg-gradient-to-b from-[#2d2d3a] via-[#2b2b35] to-[#2b2b35] p-[30px] mb-[30px] shadow-md  relative after:content-[''] after:w-[10px] after:h-[10px] after:bg-[rgba(43,43,53,0.98)] after:-rotate-[-135deg] after:absolute after:top-[11px] after:right-[-5px] after:border-[transparent] after:border-t-[10px] after:border-r-[10px] after:rounded-tr-[50%]">
-                <div className="flex justify-between w-full">
+              <div className=" md:mr-[40px] mr-[30px] bg-gradient-to-b from-[#2d2d3a] via-[#2b2b35] to-[#2b2b35] p-[30px] mb-[30px] shadow-md  relative after:content-[''] after:w-[10px] after:h-[10px] after:bg-[rgba(43,43,53,0.98)] after:-rotate-[-135deg] after:absolute after:top-[11px] after:right-[-5px] after:border-[transparent] after:border-t-[10px] after:border-r-[10px] after:rounded-tr-[50%]">
+                <div className="flex flex-wrap justify-between w-full">
                   <div className="">
                     <h5 className="text-[14px] font-semibold text-[#FAFAFC]">
                       Tittle A Section 1
@@ -45,7 +45,7 @@ export function MyBackgroundsection() {
               <div className="bg-[#fafafc] absolute top-4 right-[-4px] w-[23px] h-[23px] opacity-0 rounded-full transition duration-400 ease-in-out"></div>
               <div className="absolute top-[8px] right-0 w-[15px] h-[15px] rounded-full border-[3px] border-solid border-[#ffc107] bg-[#1e1e28] shadow-md"></div>
               <div className=" mr-[40px] bg-gradient-to-b from-[#2d2d3a] via-[#2b2b35] to-[#2b2b35] p-[30px] mb-[30px] shadow-md  relative after:content-[''] after:w-[10px] after:h-[10px] after:bg-[rgba(43,43,53,0.98)] after:-rotate-[-135deg] after:absolute after:top-[11px] after:right-[-5px] after:border-[transparent] after:border-t-[10px] after:border-r-[10px] after:rounded-tr-[50%]">
-                <div className="flex justify-between w-full">
+                <div className="flex flex-wrap justify-between w-full">
                   <div className="">
                     <h5 className="text-[14px] font-semibold text-[#FAFAFC]">
                       Tittle A Section 1
@@ -74,7 +74,7 @@ export function MyBackgroundsection() {
             </div>
           </div>
         </div>
-        <div className="lg:w-1/2 flex-grow-0 flex-shrink-0 flex-basis-1/2 max-w-[50%]">
+        <div className="lg:w-1/2 flex-grow-0 flex-shrink-0 flex-basis-1/2 max-w-[100%] md:max-w-[50%]">
           <div className="self-center relative mb-[30px]">
             <h4 className="text-[14px] text-[600] text-[#fafafc]">Work Experience</h4>
           </div>
@@ -83,8 +83,8 @@ export function MyBackgroundsection() {
             <div className="relative">
               <div className="bg-[#fafafc] absolute top-4 right-[-4px] w-[23px] h-[23px] opacity-0 rounded-full transition duration-400 ease-in-out"></div>
               <div className="absolute top-[8px] right-0 w-[15px] h-[15px] rounded-full border-[3px] border-solid border-[#ffc107] bg-[#1e1e28] shadow-md"></div>
-              <div className=" mr-[40px] bg-gradient-to-b from-[#2d2d3a] via-[#2b2b35] to-[#2b2b35] p-[30px] mb-[30px] shadow-md  relative after:content-[''] after:w-[10px] after:h-[10px] after:bg-[rgba(43,43,53,0.98)] after:-rotate-[-135deg] after:absolute after:top-[11px] after:right-[-5px] after:border-[transparent] after:border-t-[10px] after:border-r-[10px] after:rounded-tr-[50%]">
-                <div className="flex justify-between w-full">
+              <div className=" md:mr-[40px] mr-[30px] bg-gradient-to-b from-[#2d2d3a] via-[#2b2b35] to-[#2b2b35] p-[30px] mb-[30px] shadow-md  relative after:content-[''] after:w-[10px] after:h-[10px] after:bg-[rgba(43,43,53,0.98)] after:-rotate-[-135deg] after:absolute after:top-[11px] after:right-[-5px] after:border-[transparent] after:border-t-[10px] after:border-r-[10px] after:rounded-tr-[50%]">
+                <div className="flex flex-wrap justify-between w-full">
                   <div className="">
                     <h5 className="text-[14px] font-semibold text-[#FAFAFC]">
                       Tittle A Section 1
@@ -115,7 +115,7 @@ export function MyBackgroundsection() {
               <div className="bg-[#fafafc] absolute top-4 right-[-4px] w-[23px] h-[23px] opacity-0 rounded-full transition duration-400 ease-in-out"></div>
               <div className="absolute top-[8px] right-0 w-[15px] h-[15px] rounded-full border-[3px] border-solid border-[#ffc107] bg-[#1e1e28] shadow-md"></div>
               <div className=" mr-[40px] bg-gradient-to-b from-[#2d2d3a] via-[#2b2b35] to-[#2b2b35] p-[30px] mb-[30px] shadow-md  relative after:content-[''] after:w-[10px] after:h-[10px] after:bg-[rgba(43,43,53,0.98)] after:-rotate-[-135deg] after:absolute after:top-[11px] after:right-[-5px] after:border-[transparent] after:border-t-[10px] after:border-r-[10px] after:rounded-tr-[50%]">
-                <div className="flex justify-between w-full">
+                <div className="flex flex-wrap justify-between w-full">
                   <div className="">
                     <h5 className="text-[14px] font-semibold text-[#FAFAFC]">
                       Tittle A Section 1

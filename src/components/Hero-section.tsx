ab5c723ebd33b2483de4f88e7d49165f11a7c434
princeajuzie7 <princeajuzie03@gmail.com>
2024-02-02 14:03:13 +0100

    feat: changes

--- src/components/Hero-section.tsx
+++ src/components/Hero-section.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import User2 from "../../public/resources/images/face.png";
 import { TypeAnimation } from "react-type-animation";
+import { IoIosArrowForward } from "react-icons/io";
 export function Herosection() {
   return (
     <div className="lg:px-8 lg:pt-8 px-0 py-0  relative w-full">
@@ -12,7 +13,7 @@ export function Herosection() {
         }}
       >
         <div className=" relative flex gap-11 px-20 py-[60px] rounded-md bg-gradient-to-r from-[rgba(45,45,58,0.9)] via-[rgba(45,45,58,0.7)] to-[rgba(43,43,53,0.7)] ">
-          <div className="w-full lg:w-[calc(100vw-585px)]" >
+          <div className="w-full lg:w-[calc(100vw-585px)]">
             <div className="  self-center flex flex-col gap-4">
               <h1 className="mb-15 text-white text-[36px]">
                 Discover my Amazing <br />
@@ -48,15 +49,19 @@ export function Herosection() {
                 </span>
                 &lt;<i className="text-[#FFC107]">code</i>&gt;
               </div>
-              <div className="art-buttons-frame">
+              <div className="art-buttons-frame flex items-center gap-3  text-center">
                 <a
                   href="#."
                   className="py-[15px] px-[35px] bg-[#FFC107] uppercase text-[#20202a] text-[700] mr-[15px]"
                 >
                   <span>Explore now</span>
                 </a>
-                <a href="#." className="art-link art-white-link art-w-chevron ">
-                  Hire me
+                <a
+                  href="#."
+                  className="flex text-[#fff] items-center gap-2 uppercase text-center  hover:transition-transform hover:duration-400 hover:ease-in-out"
+                >
+                  <span className="text-center">Hire me</span>
+                  <IoIosArrowForward />
                 </a>
               </div>
             </div>

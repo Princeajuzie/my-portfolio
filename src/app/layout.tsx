5fc88eeed188f648cadec7d1f0ef7f40ab3f94ee
princeajuzie7 <princeajuzie03@gmail.com>
2024-03-04 08:50:03 +0100

    feat: new fieat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Prince Ajuzie - Fullstack Engineer",
-  description: "Welcome to Prince Ajuzie Portfolio A Multifaceted Solopreneur | SAAS Builder in Public 🚀 | Technical Writer 📝 | Shaping tech solutions and sharing the journey.  ",
+  description:
+    "Welcome to Prince Ajuzie Portfolio A Multifaceted Solopreneur | SAAS Builder in Public 🚀 | Technical Writer 📝 | Shaping tech solutions and sharing the journey.  ",
 };
 
 export default function RootLayout({
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children} <Analytics />
+      </body>
     </html>
   );
 }

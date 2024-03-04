98cf5aa3d1d6a055d53b1e3514e84f058eb35cfd
princeajuzie7 <princeajuzie03@gmail.com>
2024-03-04 09:29:08 +0100

    feat

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  return <div>{children} </div>;
+  return <div>{children} <Analytics /> </div>;
 }
 
 export default Layout;
